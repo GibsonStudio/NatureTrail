bd48df3909760150d22eb390ff617643470a4798
Jon Williams <jon.williams@cae.com>
2018-02-21 09:16:45 +0000

    modified message box

--- application/views/message.php
+++ application/views/message.php
@@ -1,54 +1,53 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<div class="message">
+<div id="message-box-container">
+
+  <div id="message-box">
+
+    <!-- message text -->
+    <div id="message-text"><?php echo $message_text; ?></div>
+
+    <!-- buttons -->
+    <div id="buttons-container">
     <?php
-    
-    echo $message_text;
-    
+
     if (isset($buttons))
     {
-        
-        echo '<hr />';
-        
+
         foreach ($buttons as $button)
         {
-            
+
             if (isset($button['type']))
             {
-                
+
                 //back button
                 if ($button['type'] == 'back')
                 {
-                    
+
                     //is button_text set?
                     if (isset($button['button_text'])) {
                         $text = $button['button_text'];
                     } else {
                         $text = lang('back');
                     }
-                    
+
                     $back_level = 1;
-                    
-                    if (isset($button['back_level']))
-                    {
-                        $back_level = $button['back_level'];
-                    }
-                    
+                    if (isset($button['back_level'])) { $back_level = $button['back_level']; }
                     echo '<a href="javascript:history.go(-'.$back_level.');" class="button">'.$text.'</a>';
-                    
+
                 }
-                
-                
+
             }
             else
             {
                 //default button
                 echo anchor($button['link'], $button['button_text'], 'class="button"');
             }
-                        
+
         }
-        
+
     }
-    
-    ?>    
-</div>
+    ?>
+    </div> <!-- #buttons-container -->
+  </div> <!-- #message-box -->
+</div> <!-- #message-box-container -->
